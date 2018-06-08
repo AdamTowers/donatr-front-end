f8c66ba779c89e984abb3f721814c930a521c835
adamtowers <hello@adamtowers.design>
2018-06-08 10:58:32 -0400

    imported provider and create store, wrapped <App /> in <Provider >

--- src/index.js
+++ src/index.js
@@ -3,4 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+
+import manageDonor from './reducers/manageDonor';
+
+export const configureStore = () => {
+  return createStore(manageBand, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+};
+
+let store = configureStore();
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App store={store} />
+  </Provider>,
+  document.getElementById('root')
+);
