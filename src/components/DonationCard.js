3867b01e5f237aa886ae91f9d3e9b3f5b9bbc178
adamtowers <hello@adamtowers.design>
2018-06-13 17:52:00 -0400

    linked donation card in DonorAccount to corresponding fund

--- src/components/DonationCard.js
+++ src/components/DonationCard.js
@@ -1,15 +1,11 @@
 import React, { Component } from 'react';
 
 export default class DonationCard extends Component {
-  handleFundClick(event) {
-    console.log(this.props)
-  }
-
   render() {
     return (
-      <div className='card'>
-        <h3>${this.props.donation.amount}</h3>
-        <h5 onClick={(event)=>this.handleFundClick(event)}>{this.props.donation.fund_title}</h5>
+      <div className='card' onClick={() => this.props.history.push(`/funds/${this.props.donation.fund_id}`)}>
+        <h3>{this.props.donation.amount}</h3>
+        <h5>{this.props.donation.fund_title}</h5>
       </div>
     )
   }
