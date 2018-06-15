a063f71641f18ba0ccd5dcb1f74318a93940b82c
adamtowers <hello@adamtowers.design>
2018-06-15 15:34:46 -0400

    fund card image styling

--- src/components/FundCard.js
+++ src/components/FundCard.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Line } from 'rc-progress';
 
 export default class FundCard extends Component {
   handleFundClick(event) {
@@ -8,9 +9,19 @@ export default class FundCard extends Component {
   render() {
     return (
       <div className='card sm' onClick={(event) => this.handleFundClick(event)}>
+        <div className='fund-card-image-container'>
+          <img className='fund-card-image' src={this.props.fund.picture} />
+        </div>
         <h4 onClick={(event) => this.handleFundClick(event)}>{this.props.fund.title}</h4>
         <h5>{this.props.fund.organization_name}</h5>
         <p>${this.props.fund.raised}/<strong>${this.props.fund.goal}</strong> goal reached</p>
+        <Line
+          percent='10'
+          strokeWidth='6'
+          strokeColor='rgb(236, 144, 35)'
+          trailWidth='3'
+          gapDegree='360'
+          />
       </div>
     )
   }
