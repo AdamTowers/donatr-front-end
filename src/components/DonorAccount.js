776897f259094f18e0d8fb70fd347471ffd39d09
adamtowers <hello@adamtowers.design>
2018-06-20 14:42:14 -0400

    added loading icon to DonorAccount component

--- src/components/DonorAccount.js
+++ src/components/DonorAccount.js
@@ -2,8 +2,13 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux'
 import { fetchUser } from '../actions/index'
 import DonationCard from './DonationCard'
+import ReactLoading from 'react-loading';
 
 class DonorAccount extends Component {
+  state = {
+    loaded: false
+  }
+
   componentDidMount() {
     if(localStorage.getItem('token')) {
       fetch(`http://localhost:3000/api/v1/donors/${localStorage.getItem('user_id')}`, {
@@ -16,6 +21,11 @@ class DonorAccount extends Component {
        .then(json =>
          this.props.dispatch(fetchUser(json))
         )
+       .then(
+         this.setState({
+           loaded: true
+         })
+       )
     } else {
       this.props.history.push('/login')
     }
@@ -32,26 +42,31 @@ class DonorAccount extends Component {
 
     return (
       <div className='container'>
-        <div>
-          <h1 className='name white-text'>{this.props.user.first_name} {this.props.user.last_name}</h1>
-          <div className='account-buttons-container'>
-            <button className='button-lg' onClick={() => this.props.history.push('/account/edit')}>Edit Account</button>
-          </div>
-          {
-            this.props.user.donations.length > 0 ?
-            <div>
-              <h3 className='white-text'>Recent Donations</h3>
-              <div className='flex'>
-                {donations}
+        {
+          this.state.loaded ?
+          <div>
+            <h1 className='name white-text'>{this.props.user.first_name} {this.props.user.last_name}</h1>
+            <div className='account-buttons-container'>
+              <button className='button-lg' onClick={() => this.props.history.push('/account/edit')}>Edit Account</button>
+            </div>
+            {
+              this.props.user.donations.length > 0 ?
+              <div>
+                <h3 className='white-text'>Recent Donations</h3>
+                <div className='flex'>
+                  {donations}
+                </div>
               </div>
-            </div>
-            :
-            <div>
-              <p className='white-text'>You have no donations yet. <button className='button-sm'>Find Funds</button></p>
-            </div>
-          }
+              :
+              <div>
+                <p className='white-text'>You have no donations yet. <button className='button-sm'>Find Funds</button></p>
+              </div>
+            }
+          </div>
+          :
+          <ReactLoading className='loading-icon' type='spin' height='32px' width='32px' />
+        }
 
-        </div>
       </div>
     )
   }
